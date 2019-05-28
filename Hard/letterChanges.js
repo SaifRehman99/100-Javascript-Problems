44fcdcd4d2bb251efb8ae9286cbb6783c9295abc
SaifRehman99 <saifr7493@gmail.com>
2019-05-28 20:48:08 +0500

    last solution added

--- Hard/letterChanges.js
+++ Hard/letterChanges.js
@@ -2,19 +2,19 @@
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
-const letterChanges = (str)=> {
+const letterChanges = (str) => {
     let newStr = str.toLowerCase().replace(/[a-z]/gi, char => {
-      if (char === 'z' || char === 'Z') {
-        return 'a';
-      } else {
-        return String.fromCharCode(char.charCodeAt() + 1);
-      }
+        if (char === 'z' || char === 'Z') {
+            return 'a';
+        } else {
+            return String.fromCharCode(char.charCodeAt() + 1);
+        }
     });
-  
+
     newStr = newStr.replace(/a|e|i|o|u/gi, vowel => vowel.toUpperCase());
-  
+
     return newStr;
-  }
-  
-  let res = letterChanges('Saif Rehman');
-  console.log(res);
+}
+
+let res = letterChanges('Saif Rehman');
+console.log(res);
